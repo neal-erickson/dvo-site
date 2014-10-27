c00022c2d99b038a5b724f9b1894475c597df8d3
Neal Erickson <nerickson@homeaway.com>
2014-10-27 11:44:24 -0600

    swapped out test images for real stuff; created settings.js for site control

---
+++ site/js/settings.js
@@ -0,0 +1,17 @@
+// This file is used to control site settings as simply as possible
+
+// Container object
+window.siteSettings = {
+
+	// How fast (in ms) the gallery should swap images
+	gallerySwapDelay: 6000,
+
+	// Which images to display in the gallery (path from site root)
+	galleryImageUrls: [
+		"images/photo_montage_full.jpg",
+		"images/fall_colors_high_grade_wide_full.jpg",
+		"images/cody_buffalos_main_page.jpg",
+		"images/la_sals_moonrise_main_page.jpg"
+	]
+
+};
--- site/js/site.js
+++ site/js/site.js
@@ -1,20 +1,14 @@
 // This is a shortcut to "ready"
 $(function() {
+
 	// Initialize main tab
    	$("#content").tabs();
 
-   	// Get images ready
-   	//debugger;
-
-   	var imageList = [
-   		"http://www.lanzaroteoceanfilmfestival.eu/wp-content/uploads/2011/12/jellyfish_865.jpg",
-   		"http://15pictures.com/wp-content/gallery/15-pictures-jellyfish/jellyfish-8.jpg",
-   		"http://images3.alphacoders.com/207/207317.jpg",
-   		"http://hwcdn.iplayerhd.com/data/d/37f0d78d.jpg"
-   	];
+   	// These are maintained in a separate file for ease of editing
+   	var imageList = window.siteSettings.galleryImageUrls;
+   	var imageSwapTime = window.siteSettings.gallerySwapDelay;
 
    	var currentImageIndex = -1; // will increment to 0
-   	var imageSwapTime = 5000; // in ms
    	var imageElement = $('#content-gallery-image');
 
    	function setGalleryImage(){
