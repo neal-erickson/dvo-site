25313c1ce4f12d49ac2ccd1c06f0a6aafff8fd80
Neal Erickson <minus.twelve@gmail.com>
2015-12-20 13:45:40 -0700

    main background images now loading using chained async image requests; modified default background to be lower quality jpg file for brevity

--- site/js/settings.js
+++ site/js/settings.js
@@ -4,11 +4,11 @@
 window.siteSettings = {
 
 	// How fast (in ms) the gallery should swap images
-	gallerySwapDelay: 6000,
+	gallerySwapDelay: 10000,
 
 	// Which images to display in the gallery (path from site root)
 	galleryImageUrls: [
-		"images/photo_montage_full.jpg",
+		"images/photo_montage_full_export3.jpg",
 		"images/fall_colors_high_grade_wide_full.jpg",
 		"images/cody_buffalos_main_page.jpg",
 		"images/la_sals_moonrise_main_page.jpg"
--- site/js/site.js
+++ site/js/site.js
@@ -1,6 +1,7 @@
 // This is a shortcut to "ready"
 $(function() {
 
+    // Leveraging jQueryUI tabs for this
     // Initialize main tab
    	$("#content").tabs();
 
@@ -21,13 +22,10 @@ $(function() {
    	// These are maintained in a separate file for ease of editing
    	var imageList = window.siteSettings.galleryImageUrls;
    	var imageSwapTime = window.siteSettings.gallerySwapDelay;
-
-   	var currentImageIndex = -1; // will increment to 0
-   	var imageElement = $('#content-gallery-image');
-
-   	function setGalleryImage(){
-   		console.log('setGalleryImage index', currentImageIndex)
-
+   	var currentImageIndex = 0; // will increment each time, but we already start on the first, so it will be 1
+    var imageParentElement = $('#inset-content-wrapper');
+    
+   	function loadNextGalleryImage(){
    		// manage index
    		if(currentImageIndex >= imageList.length - 1){
    			currentImageIndex = 0;
@@ -35,20 +33,46 @@ $(function() {
    			currentImageIndex++;
    		}
 
-   		// set image src
+   		// Load image as a detached dom img, then set
    		var imageUrl = imageList[currentImageIndex];
-   		imageElement.attr('src', imageUrl);
-   		//console.log('image src', imageUrl)
+        
+        $.when(promiseLoadImageAsync(imageUrl))
+            .then(function (imageElement) {
+                //debugger;
+                // remove the previously existing img and replace with the newly-loaded one.
+                // the browser will cache img src loading very well
+                imageParentElement.children('img#content-gallery-image').remove();
+                imageParentElement.append(imageElement)
+            })
+            .always(function(){
+                // even if we failed to load, move on after a bit
+                triggerLoadAfterTimeout();
+            });
    	}
 
-   	// function to be called each time
-   	var intervalHandle = setInterval(setGalleryImage, imageSwapTime);
-
-   	// do it immediately first time
-   	setGalleryImage();
-
-   	// note: can call 'clearInterval(intervalHandle)' if necessary
-
+    function triggerLoadAfterTimeout(){
+        setTimeout(function(){
+            loadNextGalleryImage();
+        }, imageSwapTime);
+    }
+    
+    // loads an image into a new DOM element
+    function promiseLoadImageAsync(source) {
+        console.log('promiseLoadImageAsync', source);
+        return $.Deferred (function (task) {
+            var image = new Image();
+            image.onload = function () {task.resolve(image);}
+            image.onerror = function () {task.reject();}
+            image.src = source;
+            image.id = 'content-gallery-image';
+        }).promise();
+    }
+    
+   	// kick it of initially 
+   	triggerLoadAfterTimeout();
+   	
+    // IMAGE GALLERY LOADING:
+    
      // Going to do some tricky stuff here with image loading:
      // Each image is pointing to a placeholder, but has a data-src
      // attribute that we can use to kick off ajax requests to get the content
@@ -58,7 +82,6 @@ $(function() {
        var imageObj = $(galleryImage); // when we ran the .each, we removed the jquery context
        var imageUrl = imageObj.data('src');
        imageObj.attr('src', imageUrl);
-       console.log('setting gallery image src', imageUrl);
      });
 
 });
